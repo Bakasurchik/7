77d5aa28b031b59d7d6b249aad327e90f24a3e05
Bakasurchik <73386008+Bakasurchik@users.noreply.github.com>
2021-01-26 02:02:30 +0500

    Add files via upload

---
+++ 7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace _7
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //определяет, зарегестрирован ли источник событий на ПК
+            if (!EventLog.SourceExists("Lab7"))
+            {
+                //Задает указанное имя источника в качестве допустимого источника событий
+                EventLog.CreateEventSource("MySource", "MyNewLog");
+                Console.WriteLine("Создан источник события");
+                //Тут, как я понял, надо закрыть программу, чтобы начало работать
+                return;
+            }
+
+            //Создается экземпляр класса и назначается его источник
+            EventLog myLog = new EventLog();
+            myLog.Source = "MySource";
+
+            //Запись в журнал событий
+            myLog.WriteEntry("Запись в журнал событий");
+        }
+}
